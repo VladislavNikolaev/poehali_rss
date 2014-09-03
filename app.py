bb7b0e0693f53cbcd8c448a154dbfa17d4e4ccdf
Pavel Tysliatski <pavel.tyslyatsky@gmail.com>
2014-09-03 18:56:52 +0300

    fix feed

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def home():
 def feed():
     feed = AtomFeed('Recent Articles', feed_url=request.url, url=request.url_root)
     for topic in db.session.query(Topic).order_by(Topic.date.desc()).limit(20).all():
-        feed.add(topic.title, topic.body, content_type='html', url=topic.url, published=topic.date)
+        feed.add(topic.title, topic.body, content_type='html', url=topic.url, published=topic.date, updated=topic.date)
     db.session.rollback()
     return feed.get_response()
 
