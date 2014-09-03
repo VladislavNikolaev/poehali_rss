2fa4b989d1704a7ce5ed5de46e4328b3a583b340
Pavel Tysliatski <pavel.tyslyatsky@gmail.com>
2014-09-03 13:07:27 +0300

    add home redirection to feed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.contrib.atom import AtomFeed
 
@@ -16,8 +16,13 @@ db = SQLAlchemy(app)
 from db import Topic
 
 
+@app.route('/')
+def home():
+    return redirect(url_for('feed'))
+
+
 @app.route('/topics.atom')
-def recent_feed():
+def feed():
     feed = AtomFeed('Recent Articles', feed_url=request.url, url=request.url_root)
     for topic in db.session.query(Topic).order_by(Topic.date.desc()).limit(20).all():
         feed.add(topic.title, topic.body, content_type='html', url=topic.url, published=topic.date)
