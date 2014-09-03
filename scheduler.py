7a8ae6cd84754aceeffcfc49d4569d16a7170343
Pavel Tysliatski <pavel.tyslyatsky@gmail.com>
2014-09-03 17:59:38 +0300

    add scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from app import db
 from db import Topic
 from parser import parse_list, parse_page
 
 
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', minutes=10)
 def scrape():
     for url, title in parse_list():
         topic = db.session.query(Topic).filter(Topic.url == url).first()
@@ -15,5 +19,4 @@ def scrape():
     db.session.commit()
 
 
-if __name__ == '__main__':
-    scrape()
+sched.start()
